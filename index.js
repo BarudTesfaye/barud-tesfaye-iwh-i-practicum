be036c2b28f15540edad1a8d801bc5a4b206762b
Barud Tesfaye <barud.tesfaye@mediajunction.com>
2025-12-11 09:58:26 -0600

    Add routes for homepage and custom object updates in index.js
    - Implemented GET route for the homepage.
    - Added GET route to fetch and render company data from HubSpot API.
    - Created POST route to update custom object data and redirect to the homepage.

--- index.js
+++ index.js
@@ -12,15 +12,50 @@ const PRIVATE_APP_ACCESS = process.env.AUTH_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    const companies = 'https://api.hubspot.com/crm/v3/objects/companies';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(companies, { headers });
+        const data = resp.data.results;
+        res.render('companies', { title: 'Companies | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const update = {
+        properties: {
+            "favorite_book": req.body.newVal
+        }
+    }
+
+    const email = req.query.email;
+    const updateCompany = `https://api.hubapi.com/crm/v3/objects/companies/${email}?idProperty=email`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.patch(updateCompany, update, { headers } );
+        res.redirect('back');
+    } catch(err) {
+        console.error(err);
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
